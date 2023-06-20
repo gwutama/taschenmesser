d7d56864c8dae5655b0667285226ec3eefe2217b
Galuh Utama <galuh.utama@gwutama.de>
2023-06-20 14:22:46 +0200

    [tsm-unitctl] Added command line flags to list units

--- tsm-unitctl/src/main.rs
+++ tsm-unitctl/src/main.rs
@@ -1,6 +1,8 @@
 use tsm_ipc::{tsm_unitman_rpc, tsm_common_rpc, RpcClient};
 use protobuf::{Message, Enum};
 use log::{debug, warn};
+use argparse::{ArgumentParser, StoreTrue};
+use tabled::{builder::Builder, settings::Style};
 
 const BIND_ADDRESS: &str = "ipc:///tmp/tsm-unitman.sock";
 
@@ -10,15 +12,78 @@ fn main() {
     let env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "debug");
     env_logger::init_from_env(env);
 
-    match send_ack_request(String::from("ping")) {
-        Ok(ack_response) => debug!("Ack = Received response: {:?}", ack_response),
-        Err(error) => warn!("Ack = No response received: {}", error),
-    };
+    // parse command line arguments
+    let mut ping = false;
+    let mut list_units = false;
 
-    match send_list_units_request() {
-        Ok(list_units_response) => debug!("ListUnits = Received response: {:?}", list_units_response.units),
-        Err(error) => warn!("ListUnits = No response received: {}", error),
-    };
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Taschenmesser Unit Control");
+        ap.refer(&mut ping).add_option(&["-p", "--ping"], StoreTrue, "Test connection to unit manager");
+        ap.refer(&mut list_units).add_option(&["-l", "--list"], StoreTrue, "List units");
+        ap.parse_args_or_exit();
+    }
+
+    if ping {
+        match send_ack_request(String::from("ping")) {
+            Ok(ack_response) => println!("Received response: {:?}", ack_response.message),
+            Err(error) => warn!("No response received: {}", error),
+        };
+    } else if list_units {
+        match send_list_units_request() {
+            Ok(list_units_response) => {
+                print_units(list_units_response.units);
+            },
+            Err(error) => warn!("No response received: {}", error),
+        };
+    } else {
+        println!("No command specified. Use --help for more information.");
+    }
+}
+
+
+fn print_units(units: Vec<tsm_unitman_rpc::Unit>) {
+    let mut builder = Builder::new();
+    builder.set_header(vec!["UNIT NAME", "IS ENABLED", "RESTART POLICY", "RUN STATE", "PROBE STATE", "COMMAND"]);
+
+    for unit in units {
+        let enabled = match unit.enabled {
+            true => String::from("Enabled"),
+            false => String::from("Disabled"),
+        };
+
+        let restart_policy = match tsm_unitman_rpc::unit::RestartPolicy::from_i32(unit.restart_policy.value()) {
+            Some(policy) => match policy {
+                tsm_unitman_rpc::unit::RestartPolicy::Always => String::from("Always"),
+                tsm_unitman_rpc::unit::RestartPolicy::Never => String::from("Never"),
+            },
+            None => String::from("Unknown"),
+        };
+
+        let probe_state = match tsm_unitman_rpc::unit::ProbeState::from_i32(unit.probe_state.value()) {
+            Some(state) => match state {
+                tsm_unitman_rpc::unit::ProbeState::Undefined => String::from("Undefined"),
+                tsm_unitman_rpc::unit::ProbeState::Alive => String::from("Alive"),
+                tsm_unitman_rpc::unit::ProbeState::Dead => String::from("Dead"),
+            },
+            None => String::from("Unknown"),
+        };
+
+        let command = format!("{} {}", unit.executable, unit.arguments.join(" "));
+
+        let run_state: String = match unit.pid {
+            n if n > 0 => String::from(format!("Running (pid={})", n)),
+            _ => String::from("Stopped"),
+        };
+
+        builder.push_record([unit.name, enabled, restart_policy, run_state, probe_state, command]);
+    }
+
+    let mut table = builder.build();
+    table.with(Style::modern());
+
+    let table = table.to_string();
+    println!("{}", table);
 }
 
 
