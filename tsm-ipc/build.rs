82658bc706486a751b18b3194a938ac717b8b93a
Galuh Utama <galuh.utama@gwutama.de>
2023-07-08 11:35:45 +0200

    [tsm-ipc] Tried to use Google protoc based parser from protoc-bin-vendored crate.

--- tsm-ipc/build.rs
+++ tsm-ipc/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     protobuf_codegen::Codegen::new()
+        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
         .includes(&["schema"])
         .input("schema/tsm_common_rpc.proto")
         .input("schema/tsm_unitman_rpc.proto")
